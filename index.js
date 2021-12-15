55e51e6f019de82a7531aec1729e2f8ecc9a293d
Vi Vo <aaronvo118@gmail.com>
2021-12-14 20:48:12 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+const events = ["Surprise"]
+
+function writeCards(names, events){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return messages;
+}
+
+
+function countDown(positiveNumber){
+    while (positiveNumber >= 0) {
+        positiveNumber--;
+        console.log(positiveNumber);
+            
+    }
+}
